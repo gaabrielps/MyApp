98749f431896032b5441b9fe7fb1ac566f7b3dc9
gaabrielps <araujolopesgabriel@gmail.com>
2022-12-24 20:52:40 -0300

    creating context

--- App.tsx
+++ App.tsx
@@ -1,14 +1,28 @@
 import React from "react"
+import {StatusBar} from 'react-native'
 
 import {ThemeProvider} from 'styled-components'
 import theme from "./src/theme"
 
 import { Routes } from "./src/routes"
+import { AuthContext } from "./src/contexts/AuthContext"
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Routes />
+      <StatusBar
+      barStyle='light-content'
+      backgroundColor='transparent'
+      translucent
+      
+      />
+      <AuthContext.Provider value={{
+        id:1,
+        name:'gabriel',
+        email:'araujolopesgabriel@gmail.com'
+      }}>
+        <Routes />
+      </AuthContext.Provider>
     </ThemeProvider>
 
   )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native'
+import {useContext} from 'react'
+
+import { AuthContext } from '../contexts/AuthContext';
+
 import { AuthRoutes } from './auth.routes';
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
+  const contextData = useContext(AuthContext)
+  console.log(contextData)
   return (
     <NavigationContainer>
         <AuthRoutes />
