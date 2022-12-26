4777b1dddf252a2b667c36208e7a7e6daab23dab
gaabrielps <araujolopesgabriel@gmail.com>
2022-12-26 11:57:17 -0300

    incluindo gerenciador de token na api

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -102,6 +102,15 @@ export function AuthContextProvider({children}:AuthContextProviderProps) {
   useEffect(() =>{
     loadUserData()
   }, [])
+
+
+  useEffect(() => {
+    const subscribe = api.registerInterceptTokenManager(signOut)
+
+    return () => {
+      subscribe()
+    }
+  },[signOut])
   
     return (
         <AuthContext.Provider value={{user, getDatas, signOut}}>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,16 +1,31 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
+
+type SignOut = () => void
+
+type APIInstanceProps = AxiosInstance & {
+    registerInterceptTokenManager: (SignOut: SignOut) => () => void
+}
 
  const api = axios.create({
     baseURL: 'https://api.staging.aca.so'
-})
+}) as APIInstanceProps
 
-api.interceptors.response.use((response) =>{
-    console.log('RESPONSE =>', response)
-    return response
-}, (error) => {
-    console.log('Error =>', error)
-    return Promise.reject(error)
-});
+api.registerInterceptTokenManager = SignOut => {
+    const InterceptTokenManager = api.interceptors.response.use((response) =>{
+        console.log('RESPONSE =>', response)
+        return response
+    }, (error) => {
+        console.log('Error =>', error)
+        return Promise.reject(error)
+    });
+
+    return () => {
+        api.interceptors.response.eject(InterceptTokenManager)
+    }
+
+}
+
+
 
 
 
